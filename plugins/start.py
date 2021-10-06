cfb74488c76a2b3358e0c9a181fffc6f3631cb56
Ahmedm4x <90485375+Ahmedm4x@users.noreply.github.com>
2021-10-06 23:55:50 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,6 +8,9 @@ async def start(client, message):
         [InlineKeyboardButton("Channel", url="https://t.me/LanaDelReyEN")],
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/ccc_l")]
+
+        [InlineKeyboardButton("Spotify Bot", url="https://t.me/SpotifyDownloadderBot")],
+
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
